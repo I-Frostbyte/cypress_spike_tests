7e2f1f48f20fbc1e48f483089cd97e484a9a0f3e
I-Frostbyte <irefyre@gmail.com>
2024-07-05 08:41:38 +0100

    Adjusting to point at hosted frontend

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,12 +1,12 @@
 describe('login Page', () => {
     it('should redirect to the Success Page', () => {
-      cy.visit('http://localhost:3000')
+      cy.visit('https://cypress-spike.onrender.com/')
       cy.get('#sign-in').click();
       cy.url().should('match', /\/success$/)
     })
 
     it('should redirect to the Gg Page', () => {
-      cy.visit('http://localhost:3000')
+      cy.visit('https://cypress-spike.onrender.com/')
       cy.get('#gg').click();
       cy.url().should('match', /\/google$/)
     })
